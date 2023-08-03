a96138e03039e3a614894615a7220953409fac3c
Dev-Tims0208 <eroy.timothy@gmail.com>
2023-08-03 22:34:57 +0800

    Add project files.

---
+++ GeomappingDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeomappingDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
